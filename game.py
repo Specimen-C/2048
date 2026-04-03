8c16d4cf1b4ea2ef5e7a5d062f1538670aee1b2c
Han <hanc26@gwmail.gwu.edu>
2026-04-03 10:57:52 -0400

    added a tile generation method using random and a method that generates a nxn board

--- game.py
+++ game.py
@@ -1,6 +1,60 @@
 import pygame
+import random           #for random tiles
 
 DIRECTIONS = ["left, right, up, down"]
+
+#spawns a random tile between 2, 4, and 8 into the board (with probability 0.7, 0.25, 0.05 respectively)
+# returns a true/false, if false, then it failed to generate a tile, which should be a lose-state
+def spawn_tile(board):
+    if not board:       #the board doesn't exist
+        return False
+
+    #This stores the list of empty spaces (location (r, c)'s) left on the baord
+    empty_spaces = []
+
+    for r in range(len(board)):
+        for c in range(len(board[r])):
+            if board[r][c] == 0:
+                empty_spaces.append((r, c))
+
+    if not empty_spaces:    # no empty cell left, so we failed to spawn a tile
+        return False
+
+    #Randomly choose a tile from empty_Spaces to add a tile to
+    row, col = random.choice(empty_spaces)
+    
+    rand = random.uniform(0, 1)
+    
+    #geneerate the value of said tile; P(2) = 0.7, P(4) = 0.25, P(8) = 0.05. Change this as needed
+    val = 0
+    if rand <= 0.7:
+        val = 2
+    elif rand > 0.7 and rand <= 0.95:
+        val = 4
+    elif rand > 0.95:
+        val = 8
+    else:
+        val = 0 #never happening but what if you know
+    board[row][col] = val
+
+    #Tile generation succeeded
+    return True
+
+
+#This method creates an empty nxn board, returned as a list [ [], [], [], [] ]
+#If someone made this already just delete it
+def create_board(n):
+    board = []
+
+    for r in range(n):
+        row = [0] * n       #a list of 0 n times
+        board.append(row)
+        
+    #spawn two initial tiles; we can move this to scale with the size of the board later
+    spawn_tile(board)
+    spawn_tile(board)
+    
+    return board
 
 def main():
     pygame.init()
